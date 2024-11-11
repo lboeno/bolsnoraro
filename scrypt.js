3a21ef3498e1dcc66a16fda2c8d124dd63c5c95c
lboeno <l.ff.boeno39@gmail.com>
2024-11-11 20:10:48 -0300

    sayjin

---
+++ scrypt.js
@@ -0,0 +1,5 @@
+// Função para mudar o texto quando o botão for clicado
+function mudarTexto() {
+    document.getElementById("titulo").innerText = "Texto alterado com sucesso!";
+    document.getElementById("botao").innerText = "Você clicou!";
+}
